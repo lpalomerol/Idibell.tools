20731c5656f3326d5478a63b37c749379b5d348b
luisp <luis.palomero.lopez.armentia@gmail.com>
2016-04-28 18:14:33 +0200

    Move tool gene_expression_data_filter and change first column filter from Hybridization to bcr_patient_barcode

---
+++ TCGA.analysis/tools/gene_expression_data_filter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#The objective of this script is remove the unnecessary gene values from the
+#  gene expression data matrix in order to simplify R scripts.
+
+DATA_FILE=$1
+GENE_LIST=$2 #Gene list can be one item or multiple, for example '"ACTA"' or '"ACTA\|VIM"'
+OUTPUT=$3
+
+GENE_LIST="bcr_patient_barcode\|$GENE_LIST"
+echo "Filtering $GENE_LIST at $DATA_FILE"
+grep $GENE_LIST $DATA_FILE > $OUTPUT
+echo "Done!, check $OUTPUT"
