1e4c40e85f67400a1af3f2cfe4d61883858e51ea
luisp <luis.palomero.lopez.armentia@gmail.com>
2016-05-03 14:47:30 +0200

    Create "followup_data" file loaders, considering the version data

--- TCGA.analysis/tools/follow_up_dataframe_tools.R
+++ TCGA.analysis/tools/follow_up_dataframe_tools.R
@@ -1,9 +1,40 @@
-load_follow_up_data <- function(file){
+#CONSTANTS
+
+v1_5 = '1.5'
+v2_1 = '2.1'
+v4_0 = '4.0'
+
+load_follow_up_data <- function(file, version='', new_time_event_file = ''){
+  if(version == ''){
+    stop('Undefined version')
+  } else if (version == v1_5 || version == v2_1){
+    followup_file = build_followup_file(file)
+  } else if (version == v4_0){
+    followup_file = build_followup_file_v4_0(file, new_time_event_file)
+  } else{
+    stop('Invalid version')
+  }
+  normalize_followup_dataframe(followup_file)
+}
+
+build_followup_file <- function(filename){
   raw = readLines(file)
   #Remove unwanted headers
   raw = raw[-2:-3]
-  dataframe = read.csv(textConnection(raw), header = TRUE, stringsAsFactors = FALSE, sep='\t')
-  keeps = c('bcr_patient_uuid', 'bcr_patient_barcode', 'new_tumor_event_dx_days_to')
+  read.csv(textConnection(raw), header = TRUE, stringsAsFactors = FALSE, sep='\t')
+}
+
+build_followup_file_v4_0 <- function(followup_file, nte_file){
+  merge(
+    build_followup_file(followup_file),
+    build_followup_file(nte_file),
+    col='bcr_patient_barcode',
+    all= TRUE
+  )
+}
+
+normalize_followup_dataframe <- function(dataframe){
+  keeps = c('bcr_patient_uuid', 'bcr_patient_barcode', 'new_tumor_event_dx_days_to', 'vital_status', 'last_contact_days_to')
   dataframe[,(names(dataframe) %in% keeps)]
 }
 
@@ -17,3 +48,4 @@ clean_follow_up_dataframe_data <- function(dataframe){
   dataframe = with(dataframe, dataframe[order(bcr_patient_barcode, new_tumor_event_dx_days_to, bcr_patient_uuid),])
   dataframe[!duplicated(dataframe$bcr_patient_barcode),]
 }
+
