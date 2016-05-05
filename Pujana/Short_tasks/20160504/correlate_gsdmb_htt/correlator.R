de993b271e0dcbbd6cee0ce449558b737824437d
luisp <luis.palomero.lopez.armentia@gmail.com>
2016-05-05 13:04:12 +0200

    Add correlation exercises

--- Pujana/Short_tasks/20160504/correlate_gsdmb_htt/correlator.R
+++ Pujana/Short_tasks/20160504/correlate_gsdmb_htt/correlator.R
@@ -13,3 +13,13 @@ tma_all$HTT_g2 = ifelse(tma_all$HTT <= 0, 0, 1)
 cor.test(tma_all$HTT, tma_all$GSDMB_g)
 cor.test(tma_all$HTT_g, tma_all$GSDMB_g)
 cor.test(tma_all$HTT_g2, tma_all$GSDMB_g)
+
+#Contingency table for group (0-1, 2)
+(conting.group1 = table(tma_all$HTT_g, tma_all$GSDMB_g))
+#Contingency table for group (0, 1-2)
+(conting.group2 = table(tma_all$HTT_g2, tma_all$GSDMB_g))
+
+#Fisher test for group(0-1,2)
+fisher.test(conting.group1)
+#Fisher test for group (0, 1-2)
+fisher.test(conting.group2)
