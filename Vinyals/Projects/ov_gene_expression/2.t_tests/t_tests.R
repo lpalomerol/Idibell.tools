01fdb94454528722773a58899ccac23721d13c19
luisp <luis.palomero.lopez.armentia@gmail.com>
2016-05-13 13:05:08 +0200

    Also tests mutated patients

--- Vinyals/Projects/ov_gene_expression/2.t_tests/t_tests.R
+++ Vinyals/Projects/ov_gene_expression/2.t_tests/t_tests.R
@@ -18,27 +18,6 @@ clinical_data.filter <- function(clinical_data){
 }
 
 
-clinical_data_file = paste(DATA_FOLDER_DIRECTORY, '/clinical.txt', sep='')
-clinical_data <- clinical_data.filter(
-  clinical_data.loader(clinical_data_file)
-)
-
-expression_data_file <-   paste(DATA_FOLDER_DIRECTORY, '/expression.subset.txt', sep='')
-expression_data <- load_gene_expression_data(expression_data_file,sep=' ')
-rownames(expression_data) <- expression_data$bcr_patient_barcode
-
-barcodes = expression_data$bcr_patient_barcode
-expression_data = lapply(expression_data, function(row){as.numeric(as.character(row))})
-expression_data$bcr_patient_barcode = barcodes 
-
-all_data= merge(clinical_data, expression_data, by="bcr_patient_barcode")
-
-all_data$BRCA2_tertile_group = cut(
-  all_data$BRCA2,
-  breaks = quantile(all_data$BRCA2, probs=seq(0,1, by=0.33), na.rm =TRUE), 
-  labels=c('LOW','MED', 'HIGH'),
-  include.lowest= TRUE
-)
 
 compare_groups <- function(groupA, groupB){
   t.test(groupA, groupB)
@@ -76,21 +55,26 @@ create_groups <- function(data, discriminating,  groupA_labels, groupB_labels){
 
 retrieve_p_values <- function(all_data){
   output <- data.frame(
+    shapiro = numeric(0),
     vascular_invasion_t = numeric(0),
     vascular_invasion_p = numeric(0),
     lypmhovascular_invasion_t = numeric(0),
     lypmhovascular_invasion_p = numeric(0),
-        # tumor_grade = numeric(0),
+    tumor_grade_t = numeric(0),
+    tumor_grade_g = numeric(0),
     clinical_stage_t = numeric(0),
     clinical_stage_p = numeric(0),
     brca_tertile_t = numeric(0),
-    brca_tertile_p = numeric(0)
+    brca_tertile_p = numeric(0),
+    brca_mutant_t = numeric(0),
+    brca_mutant_p = numeric(0)
   )
   
   expression_matrix = all_data[,
     !(colnames(all_data) %in% c('bcr_patient_barcode', 'tumor_grade', 
                       'vascular_invasion_indicator', 'clinical_stage',
-                      'lymphovascular_invasion_indicator', 'BRCA2_tertile_group'))
+                      'lymphovascular_invasion_indicator', 
+                      'BRCA2_tertile_group', 'BRCA2_mutant'))
   ]
 
   cols = colnames(expression_matrix)
@@ -107,46 +91,101 @@ retrieve_p_values <- function(all_data){
     groups_lymphovascular = create_groups(expression_matrix[,i],
                                           all_data[,'lymphovascular_invasion_indicator'], 
                                           c('YES'), 
-                                          c('NO') )    
+                                          c('NO') 
+    )
+    
     groups_clinical_stage  = create_groups(expression_matrix[,i],
                                            all_data[,'clinical_stage'], 
                                            c('Stage IA', 'Stage IB', 'Stage IC'), 
                                            c('Stage IV')
     )
-    # groups_tumor_grade = create_groups(all_data[,i],
-    #                                    all_data[,'tumor_grade'], 
-    #                                    'G1', 
-    #                                    'G4')
+
+    groups_tumor_grade = create_groups(expression_matrix[,i],
+                                       all_data[,'tumor_grade'], 
+                                       c('G1'), 
+                                       c('G3','G4')
+    )
+
     groups_brca_tertile  = create_groups(expression_matrix[,i],
-                                           all_data[,'BRCA2_tertile_group'], 
-                                           c('LOW'), 
-                                           c('HIGH')
+                                         all_data[,'BRCA2_tertile_group'], 
+                                         c('LOW'), 
+                                         c('HIGH')
     )    
-    
+
+    groups_brca_mutant  = create_groups(expression_matrix[,i],
+                                        all_data[,'BRCA2_mutant'], 
+                                        c('NO'), 
+                                        c('YES')
+    )  
+        
+    shapiro = shapiro.test(expression_matrix[,i])
     vascular_invasion_test = compare_groups(groups_vascular$groupA, 
                                             groups_vascular$groupB)
     lymphovascular_invasion_test = compare_groups(groups_lymphovascular$groupA, 
                                                   groups_lymphovascular$groupB)
     clinical_stage_test = compare_groups(groups_clinical_stage$groupA, 
                                          groups_clinical_stage$groupB)
+    tumor_grade_test = compare_groups(groups_tumor_grade$groupA, 
+                                      groups_tumor_grade$groupB)
     brca_tertile_test = compare_groups(groups_brca_tertile$groupA, 
                                        groups_brca_tertile$groupB)
-   
+    brca_mutant_test = compare_groups(groups_brca_mutant$groupA, 
+                                      groups_brca_mutant$groupB)
+
     output[name,] = c(
+      shapiro = shapiro$p.value,
       vascular_invasion_t = vascular_invasion_test$statistic,
       vascular_invasion_p = vascular_invasion_test$p.value,
       lymphovascular_invasion_t =lymphovascular_invasion_test$statistic,
       lymphovascular_invasion_p =lymphovascular_invasion_test$p.value,
       clinical_stage_t =clinical_stage_test$statistic,
       clinical_stage_p =clinical_stage_test$p.value,
-            # tumor_grade =
+      tumor_grade_t =tumor_grade_test$statistic,
+      tumor_grade_p =tumor_grade_test$p.value,
       brca_tertile_t = brca_tertile_test$statistic,
-      brca_tertile_p = brca_tertile_test$p.value
-      )
+      brca_tertile_p = brca_tertile_test$p.value,
+      brca_mutant_t = brca_mutant_test$statistic,
+      brca_mutant_p = brca_mutant_test$p.value
+    )
 
   }  
    output
 }
 
 
-head(retrieve_p_values(all_data))
+#Read and prepare clinical_data file
+clinical_data_file = paste(DATA_FOLDER_DIRECTORY, '/clinical.txt', sep='')
+clinical_data <- clinical_data.filter(
+  clinical_data.loader(clinical_data_file)
+)
+
+#Read and prepare expression data file
+expression_data_file <-   paste(DATA_FOLDER_DIRECTORY, '/expression.subset.txt', sep='')
+expression_data <- load_gene_expression_data(expression_data_file,sep=' ')
+rownames(expression_data) <- expression_data$bcr_patient_barcode
+
+barcodes = expression_data$bcr_patient_barcode
+expression_data = lapply(expression_data, function(row){as.numeric(as.character(row))})
+expression_data$bcr_patient_barcode = barcodes 
+
+#Read and prepare BRCA2 mutated data file
+mutated_brca <- read.csv(
+  paste(DATA_FOLDER_DIRECTORY, '/brca2_mutated.txt', sep='')
+)
+
+#Merge all data files and add BRCA2 expression columns
+all_data= merge(clinical_data, expression_data, by="bcr_patient_barcode")
+
+all_data$BRCA2_tertile_group = cut(
+  all_data$BRCA2,
+  breaks = quantile(all_data$BRCA2, probs=seq(0,1, by=0.33), na.rm =TRUE), 
+  labels=c('LOW','MED', 'HIGH'),
+  include.lowest= TRUE
+)
+
+all_data$BRCA2_mutant = ifelse(
+  all_data$bcr_patient_barcode %in% mutated_brca$bcr_patient_barcode, 'YES', 'NO'
+)
+
+tests = (retrieve_p_values(all_data))
+write.csv(tests, './output/t_tests.txt')
