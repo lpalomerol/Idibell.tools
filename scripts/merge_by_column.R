77f4f295d27fee38066cbbad95b1dac7fffc000c
luisp <luis.palomero.lopez.armentia@gmail.com>
2016-06-01 17:57:25 +0200

    Now merger file has python-like params

--- scripts/merge_by_column.R
+++ scripts/merge_by_column.R
@@ -1,18 +1,54 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
+library("optparse")
 
 #This file merges two files by a common column and stores the result in a third file
 
-FILE_A = args[1] # Location of first file to merge
-FILE_B = args[2] # Location of second file to merge
-COLUMN = args[3] # Column to merge 
-OUTPUT_FILE = args[4] #Location of result file
+merge_ignoring <- function(file_a, file_b, by, all){
+  file_a[by]=toupper(as.character(file_a[[by]]))
+  file_b[by]=toupper(as.character(file_b[[by]]))
+  merge(file_a, file_b, by=by, all=all)
 
-file_a = read.csv(FILE_A, sep=';')
-file_b = read.csv(FILE_B, sep=';')
+}
 
-merged = merge(file_a, file_b, by=COLUMN, all = TRUE)
-write.table(merged,
-            OUTPUT_FILE, row.names = FALSE, sep=';')
+option_list = list(
+  make_option(c('-l', '--left'), type='character', default=NULL,
+              help='First file to merge', metavar='character'),
+  make_option(c('-r', '--right'), type='character', default=NULL,
+              help='Second file to merge', metavar='character'),
+  make_option(c('-o', '--output'), type='character', default=NULL,
+              help='Output file', metavar='character'),
+  make_option(c('-c', '--column'), type='character', default=NULL,
+              help='Common column to merge', metavar='character'),
+  make_option(c('-i', '--ignore_case'), action='store_true', default=FALSE,
+              help="Merge columns ignoring casing")
+
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+FILE_A = opt$left # Location of first file to merge
+FILE_B = opt$right # Location of second file to merge
+COLUMN = opt$column# Column to merge
+OUTPUT_FILE = opt$output #Location of result file
+IGNORE_CASE= opt$ignore_case # Ignore character casing when merge
+
+if(is.null(COLUMN)){
+  stop("COLUMN field is necessary")
+} else if(is.null(OUTPUT_FILE)){
+  stop("OUTPUT field is necessary")
+}
+
+
+file_a = read.csv(FILE_A, sep=';', as.is=TRUE)
+file_b = read.csv(FILE_B, sep=';', as.is=TRUE)
+
+if(IGNORE_CASE){
+  merged = merge_ignoring(file_a, file_b, by=COLUMN, all=TRUE )
+} else {
+  merged = merge(file_a, file_b, by=COLUMN, all = TRUE)
+}
+
+write.csv(merged, OUTPUT_FILE)
 
 print(paste('Ok, file generated at', OUTPUT_FILE))
---
+++ scripts/merge_by_column_old.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+#This file merges two files by a common column and stores the result in a third file
+
+FILE_A = args[1] # Location of first file to merge
+FILE_B = args[2] # Location of second file to merge
+COLUMN = args[3] # Column to merge 
+OUTPUT_FILE = args[4] #Location of result file
+
+file_a = read.csv(FILE_A, sep=';')
+file_b = read.csv(FILE_B, sep=';')
+
+merged = merge(file_a, file_b, by=COLUMN, all = TRUE)
+write.csv(merged,
+            OUTPUT_FILE)
+
+print(paste('Ok, file generated at', OUTPUT_FILE))
