19ea0a48a5ccdd65797cca09c2abdd5ecc158118
luisp <luis.palomero.lopez.armentia@gmail.com>
2016-06-10 10:37:28 +0200

    Add optional header paremeter to gene_expression_data_filter

--- TCGA.analysis/tools/gene_expression_data_filter.sh
+++ TCGA.analysis/tools/gene_expression_data_filter.sh
@@ -6,8 +6,10 @@
 DATA_FILE=$1
 GENE_LIST=$2 #Gene list can be one item or multiple, for example '"ACTA"' or '"ACTA\|VIM"'
 OUTPUT=$3
+HEADER=${4-bcr_patient_barcode}
 
-GENE_LIST="bcr_patient_barcode\|$GENE_LIST"
+GENE_LIST="$HEADER\|$GENE_LIST"
+
 echo "Filtering $GENE_LIST at $DATA_FILE"
 grep -w $GENE_LIST $DATA_FILE > $OUTPUT
 echo "Done!, check $OUTPUT"
